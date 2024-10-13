30ec8778d2eb6a5f80c0d19c3bb605a5e391c77d
Nestor Matias <nestormatias@Nestors-MBP.home>
2024-10-13 13:59:12 +0200

    add helper config

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import { MockV3Aggregator } from "../test/mocks/MockV3Aggregator.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -10,12 +13,23 @@ contract HelperConfig is Script {
         address wbtcUsdPriceFeed;
         address weth;
         address wbtc;
-        uint256 deployKey;
+        uint256 deployerKey;
     }
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 1000e8;
+    uint256 public DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     NetworkConfig public activeNetworkConfig;
 
-    constructor() {}
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }else{
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
 
     function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return
@@ -32,5 +46,28 @@ contract HelperConfig is Script {
         if (activeNetworkConfig.wbtcUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
+
+        vm.startBroadcast();
+
+        MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            ETH_USD_PRICE
+        );
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+
+        MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            ETH_USD_PRICE
+        );
+        ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+        vm.stopBroadcast();
+
+        return NetworkConfig({
+            wethUsdPriceFeed: address(ethUsdPriceFeed),
+            wbtcUsdPriceFeed: address(btcUsdPriceFeed),
+            weth: address(wethMock),
+            wbtc: address(wbtcMock),
+            deployerKey: DEFAULT_ANVIL_KEY
+        });
     }
 }
