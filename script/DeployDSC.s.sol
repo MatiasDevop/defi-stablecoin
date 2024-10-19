bfb31134a114d5597e64892ccd3cc9ac0a6f22c2
Nestor Matias <nestormatias@Nestors-MBP.home>
2024-10-19 12:31:18 +0200

    fix lib contracts

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -5,9 +5,17 @@ pragma solidity ^0.8.19;
 import { Script } from "forge-std/Script.sol";
 import { DSCEngine } from "../src/DSCEngine.sol";
 import { DecentralizedStableCoin } from "../src/DecentralizedStableCoin.sol";
+import { HelperConfig } from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+    
     function run() external returns(DecentralizedStableCoin, DSCEngine){
+        HelperConfig config = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) = config.activeNetworkConfig();
+
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
         // DSCEngine engine = new DSCEngine
--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -44,7 +44,7 @@ contract DecentralizedStableCoin is ERC20Burnable, Ownable {
     error DecentralizedStableCoin__BurnAmountExceedsBalance();
     error DecentralizedStableCoin_NotZeroAddress();
 
-    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable() {}
 
     function burn(uint256 _amount) public override onlyOwner {
         uint256 balance = balanceOf(msg.sender);
