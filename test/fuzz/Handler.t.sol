714a9dc8dfbd67344b18228a648d76c4dd248b7f
Nestor Matias <nestormatias@Nestors-MBP.home>
2024-11-16 13:31:27 +0100

    add invariants

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,31 @@
-// Handler is going to narrow down
+// SPDX-License-Identifier: MIT
+// Handler is going to narrow down the way we call function
+
+pragma solidity ^0.8.19;
+
+import { Test } from "forge-std/Test.sol";
+import { DSCEngine } from "../../src/DSCEngine.sol";
+import { DecentralizedStableCoin } from "../../src/DecentralizedStableCoin.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+
+contract Handler is Test {
+    
+    DSCEngine dsce;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc){
+        dsce = _dscEngine;
+        dsc = _dsc;
+    }
+
+    //redeem collateral <-
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+       // dsce.depositCollateral(collateral, amountCollateral);
+    }
+
+    // Helper Functions
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns(ERC20Mock) {
+        
+    }
+}
+
